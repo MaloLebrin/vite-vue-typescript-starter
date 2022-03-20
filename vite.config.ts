ed9598a13ba31b3ea0559a81c8216c909b1887b3
malolebrin <malolebrin@gmail.com>
2022-03-20 15:38:04 +0100

    feat: add default alias

--- vite.config.ts
+++ vite.config.ts
@@ -101,6 +101,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': `${path.resolve(__dirname, 'src')}/`,
+      '~assets': `${path.resolve(__dirname, 'src', 'assets')}/`,
+      '~/': `${path.resolve(__dirname, '.')}/`,
     },
   },
 })
